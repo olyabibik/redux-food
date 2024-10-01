68ff65cd9c8483ac85c9ca519119bd2108185e3c
olyabibik <olyabibik@gmail.com>
2024-10-01 10:52:04 -0400

    modified CartItem

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,10 +1,13 @@
+import dataDishes from "../../data/dataDishes";
 
 
 const CartItem = ({cartItem}) => {
-    console.log(cartItem)
+    const dishes = dataDishes.find(item => item.id === cartItem.dishId)
+    console.log(dishes)
     
 return(<div>
-        <p>   portion(s)</p> 
+        <p> {dishes.name} </p>
+        <p> {cartItem.quantity} portion(s)</p> 
     </div>)
 }
 
